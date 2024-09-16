2997b9cd12c485481d51b1e887972d76fe304cde
ADARSH KUMAR GUPTA <106335949+AdarshGzz@users.noreply.github.com>
2024-09-16 13:12:13 +0530

    frontend newsletter integration

--- app/api/newsletter/core.ts
+++ app/api/newsletter/core.ts
@@ -5,7 +5,7 @@ export const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASS
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASSWORD
     }
 });
