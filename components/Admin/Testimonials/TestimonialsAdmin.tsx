5b79fba34819adc766a52cb8ee93a6e3b37971a7
gauravdubey013 <dubeygaurav520@gmail.com>
2024-09-24 20:00:46 +0530

    upd - dashboard & customers with charts, and faqs page and main page...

---
+++ components/Admin/Testimonials/TestimonialsAdmin.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import TestimonialCards from "./TestimonialCards";
+import TestimonialsModal from "./TestimonialsModal";
+
+const TestimonialsAdmin = () => {
+  return (
+    <div className="bg-white h-screen text-black p-10">
+      <div className="text-3xl">Testimonials</div>
+      <div className="pt-10 flex flex-row items-center justify-between">
+        <p className="text-lg"> Total Testimonials: 03</p>
+
+        {/* Wrapping Button with DialogTrigger for TestimonialsModal */}
+        <TestimonialsModal variant="create" />
+      </div>
+      <div className="pt-10 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div>
+          <TestimonialCards />
+        </div>
+        <div>
+          <TestimonialCards />
+        </div>
+        <div>
+          <TestimonialCards />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialsAdmin;
