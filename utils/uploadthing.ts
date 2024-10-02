f0eec686e9dd77c36c551ce84ec4dc2798bf4a21
gauravdubey013 <dubeygaurav520@gmail.com>
2024-10-02 12:20:26 +0530

    upd - admin.. testimonials.. products....

--- utils/uploadthing.ts
+++ utils/uploadthing.ts
@@ -1,15 +1,13 @@
+import { OurFileRouter } from "@/app/api/uploadthing/core";
 import {
-    generateUploadButton,
-    generateUploadDropzone,
+  generateUploadButton,
+  generateUploadDropzone,
 } from "@uploadthing/react";
-
-import type { OurFileRouter } from "@/app/api/testimonials/uploadthing/core";
 
 export const UploadButton = generateUploadButton<OurFileRouter>();
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
 
-
 import { generateReactHelpers } from "@uploadthing/react/hooks";
 
-
-export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>();
+export const { useUploadThing, uploadFiles } =
+  generateReactHelpers<OurFileRouter>();
