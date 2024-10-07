6962acbab2ee61177cd260de55187214991eb539
gauravdubey013 <dubeygaurav520@gmail.com>
2024-10-07 18:58:52 +0530

    upd - cloudinary....

--- app/api/testimonials/get-delete/route.ts
+++ app/api/testimonials/get-delete/route.ts
@@ -36,7 +36,7 @@ export const GET = async (request: NextRequest) => {
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
--- utils/actions/fileUpload.action.ts
+++ utils/actions/fileUpload.action.ts
@@ -58,7 +58,7 @@ export const removeMultipleFiles = async (files: string[]) => {
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
