c3014f15a8d4b77fd3fb9e709d269cf14d5a4c09
Ɗყηᴀʍɨᴄ ◠‿◕ <adarshpanditdev@gmail.com>
2024-10-07 12:22:31 +0530

    Minor Fix in responsive 3D

--- components/Textile/texture/page.tsx
+++ components/Textile/texture/page.tsx
@@ -10,7 +10,7 @@ function Texture({textures, changeTexture}: {
     <>
     <h1 className="text-3xl font-semibold p-3 text-yellow-400 mix-blend-multiply sticky top-0 z-10 backdrop-blur-sm">Texture selection</h1>
     <div className="sm:w-[90%] w-full h-12 mb-4 flex justify-between items-center gap-0 px-3">
-      <input type="text" placeholder='search...' className='w-5/6 h-4/5 py-0 rounded-xl outline-none border border-solid border-gray-300 px-4' />
+      <input type="text" placeholder='search...' className='w-5/6 h-4/5 py-0 text-base rounded-xl outline-none border border-solid border-gray-300 px-4' />
       <IoSearch className='bg-yellow-400/30 rounded-lg text-yellow-500 hover:bg-yellow-400 hover:text-white transition-all cursor-pointer p-2' size={38}/>
     </div>
       <div className="w-full h-auto flex justify-start items-start px-3 flex-wrap sm:gap-3.5 gap-2">
